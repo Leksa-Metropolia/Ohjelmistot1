ad5d0baf65c340c02f68c026a11f78c32d925fea
Lauri Lehtinen <lauri.lehtinen7@metropolia.fi>
2024-08-26 14:45:33 +0300

    mod4 korjauksia

--- mod3/teht4.py
+++ mod3/teht4.py
@@ -1,8 +1,6 @@
 v = input("Anna vuosiluku: ")
 v = int(v)
-if v % 100 == 0 and v % 400 != 0:
-    print(str (v) + " ei ole karkausvuosi")
-elif v % 4 == 0:
+if v % 4 == 0 and v % 400 != 0:
     print(str (v) + " on karkausvuosi")
 else:
-    print(int(v) + " ei ole karkausvuosi")
+    print(str(v) + " ei ole karkausvuosi")
