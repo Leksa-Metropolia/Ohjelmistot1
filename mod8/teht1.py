b332891d010e8a602d4d52e488455b8783677ab6
Lauri Lehtinen <lauri.lehtinen7@metropolia.fi>
2024-08-27 12:04:47 +0300

    mod8 tehty

--- mod8/teht1.py
+++ mod8/teht1.py
@@ -1,2 +1,19 @@
-import mariadb
+import mysql.connector
 
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='leksa',
+         password='tapani',
+         autocommit=True
+         )
+
+icao = input("Anna haettavan lentokentän ICAO-koodi\n")
+icao.upper()
+sql = f"SELECT ident, name FROM airport WHERE ident = '{icao}'"
+kurs = yhteys.cursor()
+kurs.execute(sql)
+tks = kurs.fetchall()
+for t in tks:
+    print(t[1])
