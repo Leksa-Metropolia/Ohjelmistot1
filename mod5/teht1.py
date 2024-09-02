2fec557379ffee869addc93809cc74923b3df8d3
Lauri Lehtinen <lauri.lehtinen7@metropolia.fi>
2024-09-02 13:05:27 +0300

    mod5 teht1 korjaus

--- mod5/teht1.py
+++ mod5/teht1.py
@@ -1,6 +1,8 @@
 import random
 
 i = int(input("Kuinka monta arpakuutuiota heitetään?\n"))
+summa = 0
 for j in range(0,i):
     a = random.randint(1, 6)
-    print(str(j+1) + ". arpakuution silmäluku on: " + str(a))
+    summa += a
+print(f"{i} heitetyn arpakuution summa on {summa}")
