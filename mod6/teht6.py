8caaaf42902768dea59d91689cb8bec979c3174f
Lauri Lehtinen <lauri.lehtinen7@metropolia.fi>
2024-09-05 09:38:53 +0300

    mod6 teht6 korjaus

--- mod6/teht6.py
+++ mod6/teht6.py
@@ -1,9 +1,8 @@
-import math
-
+from math import pi
 def arvo(d, h):
     r=d/2
-    a=math.pi*r**2
-    ls = a / h
+    a=pi*r**2
+    ls = h / a
     return ls
 
 d1 = int(input("Anna ensimmäisen pitsan halkaisija: "))
@@ -11,7 +10,7 @@ h1 = int(input("Anna ensimmäisen pitsan hinta: "))
 d2 = int(input("Anna toisen pitsan halkaisija: "))
 h2 = int(input("Anna toisen pitsan hinta: "))
 
-if arvo(d1, h1) < arvo(d2, h2):
+if arvo(d1, h1) > arvo(d2, h2):
     print("Toisella pitsalla on parempi hintalaatusuhde")
 else:
     print("Ensimmäisellä pitsalla on parempi hintalaatusuhde")
